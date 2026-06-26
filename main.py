236778ec4fcec517e0d4212b7ae1cbd0be154f0b
arfaoui seif <arfaouiseif1234@gmail.com>
2026-06-26 01:55:14 +0100

    testing a wrong commit 2.0

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAI()
 
 @app.get("/")
-def root():
-    return {"version": "2.0.0", "message": "Hello from the target app"}
+def ro):
+    retn {"version": "2.0.0", "message": "Hello from the target app"}
 
 @app.get("/health")
-def health():
+def health()
     return {"status": "ok"}
