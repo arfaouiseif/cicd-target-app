3e63a5f7e8f28b5dd4178de3a9d916beb7680864
arfaoui seif <arfaouiseif1234@gmail.com>
2026-07-21 14:25:27 +0100

    testing prometheus monitoring

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"version": "4.0.0", "message": "Hello from the target app"}
+    return {"version": "55.0.0", "message": "Hello from the target app and the montitoring phase :) "}
 
 @app.get("/health")
 def health():
